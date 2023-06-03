2865b65d714c4d2644f40a44291d3dfd39c563e9
DennisKeenan <dennis.k.arkananta@gmail.com>
2023-06-03 10:21:47 +0700

    Weather Predictor v2

--- Weather Predictor.py
+++ Weather Predictor.py
@@ -3,8 +3,25 @@ import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plot
 
-# Read Data
+# Read and Edit Data
+    # Read Data
 data=pd.read_csv("weather.csv")
-print(data.shape)
-print(data.head(10))
-print(data["Location"].unique())
+# print(data.shape)
+# print(data.columns)
+# print(data.info())
+# print(data["Location"].unique())
+
+    # Read Data (Specified)
+# text=[i for i in data.columns if data[i].dtype=='O']
+# print(data[text].head())
+# print(data[text].isnull().sum())
+# for i in text:
+    # print(data[i].value_counts())
+    # print(i,": ",len(data[i].unique()))
+
+    # Edit Data
+data["Date"]=pd.to_datetime(data["Date"])
+data["Year"]=data["Date"].dt.year
+data["Month"]=data["Date"].dt.month
+data["Day"]=data["Date"].dt.day
+data.drop("Date",axis=1,inplace=True)
